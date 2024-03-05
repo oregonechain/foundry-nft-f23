bcfac6d00a7d03db4b92de12a46c5571f50da55a
Oregone Chain <oregone@cha.in>
2024-03-05 17:02:01 +0200

    added some svgs

--- src/BasicNft.sol
+++ src/BasicNft.sol
@@ -3,20 +3,23 @@ pragma solidity ^0.8.18;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract BasicNFT is ERC721 {
+contract BasicNft is ERC721 {
 
     uint256 private s_tokenCounter;
+    mapping(uint256 => string) private s_tokenToUri;
 
     constructor() ERC721("Dogie", "DOG") {
         s_tokenCounter = 0;
     }
 
-    function mintNft() public {
-
+    function mintNft(string memory tokenUri) public {
+        s_tokenToUri[s_tokenCounter] = tokenUri;
+        _safeMint(msg.sender, s_tokenCounter);
+        s_tokenCounter++;
     }
 
     function tokenURI(uint256 tokenId) public view override returns(string memory) {
-        return "";
+        return s_tokenToUri[tokenId];
     }
 
 }
