a3fc75101bf13fd27aefc20445f46a8c356384df
Oregone Chain <oregone@cha.in>
2024-03-05 10:04:01 +0200

    forgot to add the file

---
+++ src/BasicNFT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract BasicNFT is ERC721 {
+
+    constructor() ERC721("Dogie", "DOG") {
+
+    }
+
+}
